70d5a90d6b0e54f81155b964a2f0b2e0254fb4f1
sachinpwr397 <79899346+sachinpwr397@users.noreply.github.com>
2024-04-05 10:01:18 +0530

    Update Teacher.java

--- src/main/java/com/driver/Teacher.java
+++ src/main/java/com/driver/Teacher.java
@@ -1,14 +1,40 @@
 package com.driver;
 
 public class Teacher {
-
     private String name;
-
     private int numberOfStudents;
-
     private int age;
 
-    public Teacher() {
+    public Teacher() {}
 
+    public Teacher(String name, int numberOfStudents, int age) {
+        this.name = name;
+        this.numberOfStudents = numberOfStudents;
+        this.age = age;
+    }
+
+    // Getters and setters
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfStudents() {
+        return numberOfStudents;
+    }
+
+    public void setNumberOfStudents(int numberOfStudents) {
+        this.numberOfStudents = numberOfStudents;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
